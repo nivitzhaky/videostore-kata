c7e4d91b8c010c329fa11da663f5ec1ded80019e
Niv.itzhaky <Niv.itzhaky@gmail.com>
2019-05-12 22:07:32 +0300

    after extracting lines header footer

--- src/main/java/video_store/Customer.java
+++ src/main/java/video_store/Customer.java
@@ -10,60 +10,75 @@ public class Customer {
         this.name = name;
     }
 
+    private String name;
+
+    private double totalAmount = 0;
+    private int frequentRenterPoints = 0;
     private ArrayList<Rental> rentals = new ArrayList<>();
     public void addRental(Rental rental) {
         rentals.add(rental);
     }
 
     public String statement() {
-        String result = "Rental Record for " + getName() + "\n";
+        return header() + rentalLines() + footer();
 
-        for  ( Rental  each  : rentals ) {
-            double thisAmount = 0;
+    }
 
-            // determines the amount for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2) {
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    }
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3) {
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    }
-                    break;
-            }
-
-            frequentRenterPoints++;
-
-            if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE
-                && each.getDaysRented() > 1)
-                frequentRenterPoints++;
-
-            result += "\t" + each.getMovie().getTitle() + "\t"
-                + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+    private String rentalLines() {
+        String lines = "";
+        for  ( Rental rental  : rentals ) {
+            lines += rentalLine(rental);
 
         }
+        return lines;
+    }
 
-        result += "You owed " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points\n";
+    private String rentalLine(Rental rental) {
+        String result = "";
+        double thisAmount = 0;
 
+        // determines the amount for each line
+        switch (rental.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (rental.getDaysRented() > 2) {
+                    thisAmount += (rental.getDaysRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += rental.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (rental.getDaysRented() > 3) {
+                    thisAmount += (rental.getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+
+        frequentRenterPoints++;
+
+        if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE
+            && rental.getDaysRented() > 1)
+            frequentRenterPoints++;
+
+        result += "\t" + rental.getMovie().getTitle() + "\t"
+            + String.valueOf(thisAmount) + "\n";
+        totalAmount += thisAmount;
         return result;
     }
 
+    private String header() {
+        return "Rental Record for " + getName() + "\n";
+    }
 
+    private String footer() {
+        String result = "";
+        result += "You owed " + String.valueOf(totalAmount) + "\n";
+        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points\n";
+        return result;
+    }
 
-    private String name;
-
-    private double totalAmount = 0;
-    private int frequentRenterPoints = 0;
     public double getTotalAmount() {
         return totalAmount;
     }
@@ -74,4 +89,5 @@ public class Customer {
     public String getName() {
         return name;
     }
+
 }
